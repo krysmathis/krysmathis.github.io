32ff47737f3c4f4257f642be014e7cd570d680b2
krysmathis <krysmathis@gmail.com>
2017-10-08 20:48:59 -0500

    completed resume object database

--- scripts/resume.js
+++ scripts/resume.js
@@ -6,37 +6,67 @@ The first step is to design what each object's properties should be. Consider yo
 */
 
 
-
+/*
 var job = {
     "headline": "",
     "company": "",
     "startDate": "",
     "endDate": "",
     "title": "",
-    "responsibilities": [],
+    "accomplishments": [],
     "skills": [],
-    "logo-url": "",
-    duration: function() {
-        
-        // The number of milliseconds in one day
-        var ONE_DAY = 1000 * 60 * 60 * 24
+    "companyLogoImg": "",
+}
+*/
 
-        // Convert both dates to milliseconds
-        var startDate_ms = this.startDate.getTime()
-        var endDate_ms = this.endDate.getTime()
-
-        // Calculate the difference in milliseconds
-        var difference_ms = Math.abs(startDate_ms - endDate_ms)
-
-        // Convert back to days and return
-        return Math.round(difference_ms/ONE_DAY)
-
-        }
+const demandPlanningDir = {
+    "headline": "Forecaster, Number-Cruncher, Leader",
+    "company": "Dollar General",
+    "startDate": new Date("2013-01-01"),
+    "endDate": new Date("2017-09-27"),
+    "title": "Director of Demand Planing",
+    "responsibilities": [
+       "The department improved forecast accuracy by 12% during my time leading the department.",
+       "Led an 8 person analytical team in providing planned and ad hoc analysis and recommendations to stakeholders ranging from Sr. Executives to Analysts.",
+       "Developed custom software application using Python and C# to cluster items based on seasonal trends which enables faster, more frequent review of item seasonality and supports advanced strategies for improving in-stock and inventory turns during seasonal periods." 
+        ],
+    "skills": ["Leadershipt", "Forecasting", "Demand Planning"],
+    "companyLogoImg": "",
 }
 
-var demandPlanningDir = Object.create(job);
-demandPlanningDir.headline = "";
-demandPlanningDir.company = "";
-demandPlanningDir.startDate = new Date("2011-06-01");
-demandPlanningDir.endDate = new Date("2011-07-01");
-console.log(demandPlanningDir.duration());
+var projectManager = {
+    "headline": "System Implementater, Project Manager",
+    "company": "Dollar General",
+    "startDate": new Date("2011-01-01"),
+    "endDate": new Date("2013-01-01"),
+    "title": "Sr. Project Manager",
+    "accomplishments": [
+        "Implemented a new Supply Chain system, GOLD, with responsibility for Master Data Management, including capturing requirements, reviewing design documents, training, and process design to support 120,000 items, 11,000 locations and 3,400 vendors.",
+        "Led GOLD user training for 200 employees from the Store Support Center and the Hong Kong office, which was highly rated by participants, based on post-event feedback surveys, and supported the timely rollout of the SCS project."
+        ],
+    "skills": ["Project Management", "User Acceptance Testing", "Design", "Training and Development"],
+    "companyLogoImg": "",
+}
+
+var strategicPlanningAndAnalysis = {
+    "headline": "Retail Scientist, Store In-stock Guru",
+    "company": "Dollar General",
+    "startDate": new Date("2009-01-01"),
+    "endDate": new Date("2010-01-02"),
+    "title": "Strategic Planning and Analysis, Sr. Manager",
+    "accomplishments": [
+        "Led over 40 business experiments for Senior Management, leveraging statistical and financial analysis to forecast the expected impact to critical financial measures, such as EBITDA, ROI and EPS.",
+        "Led the company’s Out-of-Stock Reduction initiative which worked cross-functionally to develop, test and implement solutions aimed at improving store in-stocks and increasing sales by $41.5 million."
+    ],
+    "skills": ["A B Testing", "Analysis", "Financial Planning", "ROI IRR Analysis"],
+    "companyLogoImg": "",
+}
+
+let jobs = [];
+jobs.push(demandPlanningDir, projectManager, strategicPlanningAndAnalysis);
+
+let Resume = {
+    "jobs": jobs
+}
+
+localStorage.setItem("resume", JSON.stringify(Resume));
