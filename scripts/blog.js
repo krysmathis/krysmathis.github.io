8e47a40f8cbea3d643f55365f63ef3fa8bc70d77
krysmathis <krysmathis@gmail.com>
2017-10-10 13:49:24 -0500

    prelim blog working from database

--- scripts/blog.js
+++ scripts/blog.js
@@ -18,12 +18,15 @@ const blogEntry = {
 
 const blogEntry20171006 = {
     "headline": "First Week at NSS",
-    "dateAdded": new Date("2017-10-06"),
+    "dateAdded": "2017-10-06",
     "author": "Krys Mathis",
-    "tags": ["footer","html"],
     "imgHeader": "http://media2.giphy.com/media/jrC5zowLdpw5y/giphy.gif",
-    "content": "Started my journey to becoming a software developer. Is it just me or are they stressing that we won't understand anything they say for weeks? It's nice to be around people with the same goal. We're starting at different points, but we're going to end up at the same goal.  Seems to be a lot of memes and animated GIFs going on around. Must mean we'll need a sense of humor to",
+    "tags": ["footer","html"],
+    "content": "Started my journey to becoming a software developer. Is it just me or are they stressing that we won't understand anything they say for weeks? It's nice to be around people with the same goal. We're starting at different points, but we're going to end up at the same goal.  Seems to be a lot of memes and animated GIFs going on around. Must mean we'll need a sense of humor too.",
 };
+
+// "imgHeader": 
+//
 
 let blogEntries = [];
 blogEntries.push(blogEntry20171006);
@@ -34,8 +37,59 @@ let Blog = {
 
 localStorage.setItem("blog", JSON.stringify(Blog));
 
-const blogDB = localStorage.getItem("blog");
+const blogDB = JSON.parse(localStorage.getItem("blog"));
+
+const blogs = document.getElementById("blog-posts");
 
 for (let key in blogDB) {
     const currentKey = blogDB[key];
+    console.log(key);
+    for (var i = 0; i < currentKey.length; i++) {
+        var entry = currentKey[i];
+        console.log(entry);
+        let html =  `
+            <article class="blog-post">
+                <div class="blog-header">
+                    <div class="blog-headline">${entry.headline}</div>
+                    <div class="blog-date">${(entry.dateAdded)}</div>
+                </div>
+                <div class="blog-img-header">
+                    <img src="${entry.imgHeader}">
+                </div>
+                <div class="blog-content">
+                    ${entry.content}
+                </div>
+               
+        `;
+        
+        html+=`<div class="blog-footer project-tag"><ul>`;
+        
+        let tags = entry.tags;
+        for (var i = 0; i < tags.length; i++) {
+            var currentTag = tags[i];
+            html += `<li>${currentTag}</li>`
+        }
+        
+       html += "</ul></div></article>";
+        blogs.innerHTML += html;  
+        console.log(blogs.innerHTML);
+  
+    }
 }
+
+/*
+// <article class="blog-post">
+//             <div class="blog-header">
+//                 <div class="blog-title">First Day at NSS</div>
+//                 <div class="blog-date">10/2/2017</div>
+//             </div>
+//             <div class="blog-content">
+//                 Started my journey to becoming a software developer. Is it just me or are they stressing that we won't understand anything they say for weeks? It's nice to be around people with the same goal. We're starting at different points, but we're going to end up at the same goal.
+//                 <img src="http://media2.giphy.com/media/jrC5zowLdpw5y/giphy.gif" class="blog-img float-left">
+//                 Seems to be a lot of memes and animated GIFs going on around. Must mean we'll need a sense of humor to hang with the program. WHAT DOES IT ALL MEAN?
+//             </div>
+//             <div class="blog-footer project-tag">
+//                 <ul><li>footer</li><li>html</li></ul>
+//             </div>
+//         </article>
+// */ 
