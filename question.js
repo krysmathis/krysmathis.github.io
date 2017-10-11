4b5a384edaeb33bc992c979b525d92b8a5029f8b
krysmathis <krysmathis@gmail.com>
2017-10-11 10:36:25 -0500

    working projects

---
+++ question.js
@@ -0,0 +1,15 @@
+const tags = ["html", "javascript"]
+
+let tagsHTML = "";
+for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    tagsHTML += `<li>${tag}</li>`
+    
+}
+
+let html = tags.reduce(function(l, c) {
+    l += `<li>${c}</li>`;
+    return l;
+});
+
+console.log(html);
--- scripts/projects.js
+++ scripts/projects.js
@@ -17,16 +17,82 @@ const project = {
 
 const project1 = {
     "name": "project one",
-    "dateCompleted": new Date("2017-10-06"),
+    "description": "In this project...",
+    "dateCompleted": "2017-10-06",
     "technologies": ["HTML", "CSS", "Javascript"],
     "teammates": [{"name": "Krys Mathis", "personalSite": ""}],
+    "href": "#",
     "repository": ""
 }
 
-let projects = [project1];
+const project2 = {
+    "name": "project two",
+    "description": "In this other project",
+    "dateCompleted": "2017-10-06",
+    "technologies": ["CSS", "HTML"],
+    "teammates": [{"name": "Krys Mathis", "personalSite": ""}],
+    "href": "#",
+    "repository": ""
+}
+
+let projects = [project1, project2];
 
 let Projects = {
     "projects": projects
 }
 
 localStorage.setItem("projects", JSON.stringify(Projects));
+
+const projectsDB = JSON.parse(localStorage.getItem("projects"));
+
+const projectsHTML = document.getElementById("projects");
+
+for(let key in projectsDB) {
+
+    let currentKey = projectsDB[key];
+
+    for (var i = 0; i < currentKey.length; i++) {
+        let project = currentKey[i];
+        
+        // grab the first technology listed
+        const technology = project.technologies.length>0 ? project.technologies[0] : "";
+        let tagHTML = "";
+        
+        for (let i = 0; i < project.technologies.length; i++) {
+            let tag = project.technologies[i];
+            tagHTML += `<li>${tag}</li>`   
+        }
+
+        // create the teammate string
+        let teammates = "";
+
+        for (let i = 0; i < project.teammates.length; i++) {
+            let teammate = project.teammates[i];
+            teammates += `<a href="${teammate.personalSite}">${teammate.name}</a>`;
+        }
+
+        console.log(teammates);
+
+        //let html = "";
+        projectsHTML.innerHTML += `
+            <article class="project-detail ${technology.toLowerCase()}">
+                <h3 class="project-title">${project.name}</h3>
+                <p class="project-description">${project.description}</p>
+                <p class="project-completed-date">Date completed: ${project.dateCompleted}</p>
+                <br class="project-href"><a href="${project.href}">link</a> | <a href="${project.repository}">repository</a>
+                <div class="project-tag">
+                    <p></p>
+                    <ul>
+                        ${tagHTML}
+                    </ul>
+                </div>
+            </article>
+        `
+
+        
+        
+        //+= html;
+        console.log(projectsHTML.innerHTML);
+    }
+
+}
