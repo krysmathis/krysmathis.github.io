3e64eae6c3f472e9204ed91f1798ccb1c8bb4619
krysmathis <krysmathis@gmail.com>
2017-10-18 21:54:44 -0500

    adjustments to alignment on front page

---
+++ scripts/about.js
@@ -0,0 +1,9 @@
+$('document').ready(function() {
+
+    $('#about-projects').fadeIn("slow");
+    $('#about-blog').fadeIn("slow");
+    $('#about-resume').fadeIn("slow");
+    
+});
+
+console.log("hello");
