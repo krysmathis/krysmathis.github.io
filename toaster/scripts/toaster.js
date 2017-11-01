a042e4ecb1f9f9aede2a57a7a2bc8a4eb82cf97b
krysmathis <krysmathis@gmail.com>
2017-11-01 11:37:57 -0500

    updated navbar styling to make brand white

--- toaster/scripts/toaster.js
+++ toaster/scripts/toaster.js
@@ -44,56 +44,17 @@ const Toaster = function() {
         }, timeout);
     }
 
-    return  Object.create(null, {
-        "makeToast": {value: (message,time) => {createToast(message, time)}, enumerable: true}
+    return Object.create(null, {
+        "makeToast": {
+            value: (message,time) => {createToast(message, time)},
+            enumerable: true}
     })
        
 };
 
+// to use the toaster you need the code below in your javascript
+
 let toaster = Toaster();
-
-/*
-// Get the toaster element
-const toastContainer = document.querySelector(".toaster__container");
-
-// clear the existing toaster
-while(toastContainer.hasChildNodes()){
-    toastContainer.removeChild(toastContainer.lastChild)
-}
-
-// append the ul element
-const toasterUl = document.createElement("ul");
-toasterUl.className = ("toaster")
-toastContainer.appendChild(toasterUl);
-
-
-// get the toast
-
-const createToast = function(message, timeout) {
-    
-    const toaster = document.querySelector(".toaster");
-    // get the toaster
-    const toast = document.createElement("li");
-    
-    toastMessage = document.createTextNode(message);
-    toast.appendChild(toastMessage);
-    toast.className = "toaster__toast";
-
-    if (toaster.hasChildNodes()) {
-         toaster.insertBefore(toast, toaster.firstChild);
-    } else {
-        toaster.appendChild(toast);
-    }
-
-    // set expiration timing for the toast
-    setTimeout(function() {
-        toaster.removeChild(toast);
-    }, timeout);
-}
-*/
-
-toaster.makeToast("this is a message", 10000);
-toaster.makeToast("another message", 10000);
 
 document.addEventListener("click", function(e) {
     if (e.target.id === "btn"){
